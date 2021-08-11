191708eb6a5b782ef15944ba4c0baf05681394f1
MariaJoseCandia <mariajosecandia100@gmail.com>
2021-08-11 15:33:07 -0300

    Agrege
    Agrege

--- Bolillero.Core/Class1.cs
+++ Bolillero.Core/Class1.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Bolillero.Core
 {
-    public class Class1
+    public class Bolillero
     {
+       byte cantidad{get; set;}
+       byte indiceAzar{get; set;}
+       List<byte> Afuera {get; set;}
+       List<byte> Adentro {get; set;}
+       Random r;
+       public Bolillero()
+       {
+           Afuera = new List<byte>{};
+           Adentro = new List<byte>{};
+           r = new Random();
+       }
+       public Bolillero(byte cantidad)
+       {
+           this.Llenar(this.cantidad);
+       }
+       public void Llenar(byte cantidad)
+       {
+           for(byte i=0; i < cantidad; i++)
+           {
+               Adentro.Add(i);
+           }
+       }
+       public byte SacarBolilla()
+       {
+           
+       }
     }
 }
