c8f74a7daa61a2f93ec30c1da489ac850bbbb95c
MariaJoseCandia <mariajosecandia100@gmail.com>
2021-12-03 21:25:43 -0300

    Bolillero1

---
+++ Bolillero.Core/Bolillero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolillero.Core
+{
+    public class Bolillero
+    {
+       byte Cantidad { get; set; }
+       public int CantidadFuera => Afuera.Count;
+       public int CantidadDentro => Adentro.Count;
+       List<byte> Afuera { get; set; }
+       List<byte> Adentro { get; set; }
+       Random r;
+
+       public Bolillero()
+       {
+           Afuera = new List<byte>{};
+           Adentro = new List<byte>{};
+           r = new Random(DateTime.Now.Millisecond);
+       }
+
+       public Bolillero(byte cantidad) : this() => this.Llenar(cantidad);
+
+       private void Llenar(byte cantidad)
+       {
+           for(byte i = 0; i < cantidad; i++)
+           {
+               Adentro.Add(i);
+           }
+       }
+
+       public byte SacarBolilla()
+       {
+           byte indiceAzar = (byte)r.Next(0, Adentro.Count);
+           byte bolilla = Adentro [indiceAzar];
+           Adentro.RemoveAt(indiceAzar);
+           Afuera.Add(bolilla);
+           return bolilla;
+       }
+
+       public void RellenarBolillero()
+       {
+           Adentro.AddRange(Afuera);
+           Afuera.Clear();
+       }
+
+       public bool Jugar(List<byte> jugada)
+       {
+           RellenarBolillero();
+           for(byte i = 0; i < jugada.Count; i++)
+           {
+               if(SacarBolilla() != jugada[i])
+               {
+                   return false;
+               }
+           }
+           return true;
+       }
+
+       public long jugarNVeces(List<byte> jugada, long cantidad)
+       {
+           long contador = 0;
+           for(long i = 0; i < cantidad; i++)
+           {
+               if(Jugar(jugada))
+               {
+                   contador++;
+               }
+           }
+           return contador;
+       }
+       
+    }
+}
